770938bff0e57f2116e4190f477ef942b1560aac
Jeffrey-Hamlin-V <jeffreyhamlinprojects@gmail.com>
2026-06-02 19:03:32 +0530

    Day 4

---
+++ Arrays/49_Group Anagrams.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)        
+        for w in strs:
+            c=[0]*26
+            for i in w:
+                c[ord(i)-ord("a")]+=1
+            res[tuple(c)].append(w)
+        return list(res.values())
